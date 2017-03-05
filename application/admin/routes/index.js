539eee26bbe2533325448301fcc25df0ae06b8be
songjd <310676645@qq.com>
2017-03-05 20:38:55 +0800

    '优化token验证'

--- application/admin/routes/index.js
+++ application/admin/routes/index.js
@@ -11,34 +11,9 @@ let db = require(path.join(LIB_PATH, 'db'));
 let Common = require(path.join(LIB_PATH, 'common'));
 router.use('/login', login);
 router.use('/category', (req, res, next) => {
-  let userToken = req.body.user_token || req.query.user_token;
-  if(!userToken) {
-    res.json({
-      code: 1,
-      data: {},
-      msg: 'user_token不能为空'
-    });
-    return false;
-  }
-  let sql = 'select user_name, user_token from admin where user_token = ?';
-  db.query(sql, [userToken], (error, row) => {
-    if (error) {
-      res.json(Common.json({
-        code: 1,
-        data: {},
-        msg: error
-      }));
-    }
-    if (row.length <= 0) {
-      res.json(Common.json({
-        code: 100001,
-        data: {},
-        msg: 'user_token无效'
-      }));
-    } else {
-      next();
-    }
-  });
+  Common.checkAdminToken(req, res, next);
 }, category);
-router.use('/article', article);
+router.use('/article', (req, res, next) => {
+  Common.checkAdminToken(req, res, next);
+}, article);
 module.exports = router;
